e51e4ff510bcd3650cc7a290f8038399338e48c8
zyt1487675610 <1487675610@qq.com>
2023-04-13 17:50:29 +0800

    feat: banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,9 +1,43 @@
-import React from "react";
-
-type Props = {};
-
-function Banner({}: Props) {
-  return <div>赵于婷git上传测试</div>;
+import React, { FC } from "react";
+import styles from "./Banner.module.scss";
+import Image from "next/image";
+export interface Image {
+  id: string;
+  url: string;
+  alt: string;
+  name: string;
 }
+interface Props {
+  images: Image[];
+}
+
+const Banner: FC<Props> = (props) => {
+  const { images } = props;
+
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>
+        携程美食林
+        <span className={styles.title_span}>旅行上的舌尖 美食打卡世界</span>
+      </h2>
+      <ul className={styles.imgListContainer}>
+        {images.map((image) => (
+          <div key={image.id} className={styles.imgContainer}>
+            <div className={styles.blurBox}>
+              <div className={styles.text}>{image.name}</div>
+            </div>
+            <Image
+              src={image.url}
+              alt={image.alt}
+              width={100}
+              height={150}
+              className={styles.img}
+            />
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Banner;
