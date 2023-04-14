e2ff7a5e6cb4134e8825b4bd07a3aeb296d5d6b4
zyt1487675610 <1487675610@qq.com>
2023-04-14 11:42:46 +0800

    feat: 地图组件

---
+++ pages/test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MapContainer from "../components/MapContainer";
+type Props = {};
+
+function test({}: Props) {
+  return (
+    <div>
+      <MapContainer />
+    </div>
+  );
+}
+
+export default test;
