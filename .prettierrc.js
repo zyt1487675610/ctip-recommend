ee9063cb5e1db29db893d2ee836a1b59ec6f84e0
yiyu66 <1226572422@qq.com>
2023-04-17 17:28:45 +0800

    chore:代码规范prettier配置

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 200, // 超过最大值换行
+
+  tabWidth: 2, // 缩进字节数
+
+  semi: true, // 句尾添加分号
+
+  singleQuote: false, // 使用单引号代替双引号
+
+  bracketSpacing: true, // 在对象，数组括号与文字之间加空格 "{ foo: bar }"
+}
